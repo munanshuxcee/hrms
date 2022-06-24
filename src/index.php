ad742009fd00057db3bda85821068f5c7dbc9624
Munanshu Madaan <2879@xceedance.com>
2022-06-24 17:25:32 +0530

    adding docker app

---
+++ src/index.php
@@ -0,0 +1,6 @@
+<?php
+ 
+ echo phpversion() . "ddfff ddd fffyugiugiugiug  ffff";
+
+//  docker run --name monu_cont -v /C:/Users/2879/Desktop/test_app:/var/www/html/ -p 80:80 comdockerdevenvironmentscode_server
+//  docker run -i --rm --name monu_cont1 -v //wsl$/Ubuntu/home/munanshu/www/test/:/var/www/html/ -p 80:80 php:8.0-apache
